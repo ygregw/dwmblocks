4c98762fc08863783ec847e56d3eb38c7f654bb0
Yuan1 Wang1 <ygregwxyz@gmail.com>
2024-09-26 17:09:50 -0400

    example module interval data

--- blocks.def.h
+++ blocks.def.h
@@ -1,14 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block upperblocks[] = {
    /*Icon*/   /*Command*/     /*Update-interval*/   /*Update-signal*/   /*Fixed-length*/
-	{"",       "sb-battery",   1,                    1,						-14},
-   {"",       "sb-clock",     1,                    2,                  18},
+	{"",       "sb-battery",   300,                    1,						-14},
+   {"",       "sb-clock",     30,                    2,                  18},
 };
 
 static const Block lowerblocks[] = {
-   {"",   "sb-cpu",        1,   3,   -24},
-   {"",   "sb-memory",     1,   4,   -22},
-   {"",   "sb-disk",       1,   5,   -20},
+   {"",   "sb-cpu",        3,   3,   -24},
+   {"",   "sb-memory",     3,   4,   -22},
+   {"",   "sb-disk",       10,   5,   -20},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
